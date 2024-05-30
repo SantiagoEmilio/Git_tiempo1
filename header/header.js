618a6a9507e4e61d24fa60c4b21b619f1f822cb9
Santiago <slopez@scl.edu.gt>
2024-05-29 21:56:44 -0600

    tranajo del hader

---
+++ header/header.js
@@ -0,0 +1,7 @@
+let header_DoM= document.querySelector(".header2");
+
+header_DoM.innerHTML =`
+<h1 class="titulo">comandos <samp class="o1">Git</samp></h1>
+
+<a href="https://github.com/SantiagoEmilio/Git_tiempo-/settings" class="btn-github">github</a>
+`;
